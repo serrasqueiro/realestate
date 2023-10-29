2ed574ed7207130d3c15b5d912f7d63fb724d3e7
Henrique Moreira <h@serrasqueiro.com>
2023-10-29 17:21:04 +0000

    More relaxed main/LATEST...

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 
 @echo off
 
-git pull && git submodule foreach --recursive "(git remote -v ; git pull; echo; echo ...)"
+git pull && git submodule foreach --recursive "(git remote -v ; git checkout master; git pull; echo ^^^; echo ...)"
 
 python start.py
