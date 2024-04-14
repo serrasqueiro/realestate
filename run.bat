86595b8a88518b2d1ef680bc551a6a4819873a40
Henrique Moreira <h@serrasqueiro.com>
2024-04-14 12:56:36 +0100

    Minor update

--- run.bat
+++ run.bat
@@ -4,4 +4,6 @@
 
 git pull && git submodule foreach --recursive "(git remote -v ; git checkout master; git pull; echo ^^^; echo ...)"
 
+#for X in $(cat .gitmodules | grep path.= | sed 's/.*= //'); do Y=$(pwd); gtouch $X ; echo ^__ ; cd $Y; done
+
 python start.py
